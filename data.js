9ac616e80a2be6a3294090a2ba8182f014e2a1c5
janitoess <jandmdt@gmail.com>
2026-04-30 23:45:23 -0700

    Update data.js

--- data.js
+++ data.js
@@ -4,34 +4,39 @@ const doodles = [
     value: 10,
     group: "Swarms",
     image: "https://static.wikia.nocookie.net/doodle-world/images/Snobat.png",
-    forms: ["Snobat", "Cryotera"]
+    forms: ["Snobat", "Cryotera"],
+    trend: "stable"
   },
   {
     name: "Bunsweet Line",
     value: 10,
     group: "Swarms",
     image: "https://static.wikia.nocookie.net/doodle-world/images/Bunsweet.png",
-    forms: ["Bunsweet", "Bunswirl"]
+    forms: ["Bunsweet", "Bunswirl"],
+    trend: "up"
   },
   {
     name: "Pebblett Line",
     value: 10,
     group: "Swarms",
     image: "https://static.wikia.nocookie.net/doodle-world/images/Pebblet.png",
-    forms: ["Pebblett", "Bolduo", "Monolugg"]
+    forms: ["Pebblett", "Bolduo", "Monolugg"],
+    trend: "down"
   },
   {
     name: "Fluppy Line",
     value: 10,
     group: "Swarms",
     image: "https://static.wikia.nocookie.net/doodle-world/images/Fluppy.png",
-    forms: ["Fluppy"]
+    forms: ["Fluppy"],
+    trend: "stable"
   },
   {
     name: "Wiglet Line",
     value: 10,
     group: "Swarms",
     image: "https://static.wikia.nocookie.net/doodle-world/images/Twiglet.png",
-    forms: ["Wiglet", "Tewwys Mum"]
+    forms: ["Wiglet", "Tewwys Mum"],
+    trend: "stable"
   }
 ];
