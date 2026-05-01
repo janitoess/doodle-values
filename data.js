8ade9be66983c45a2368988985b134603cef408d
janitoess <jandmdt@gmail.com>
2026-04-30 23:18:05 -0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,37 @@
+const doodles = [
+  {
+    name: "Snobat Line",
+    value: 10,
+    group: "Swarms",
+    image: "https://static.wikia.nocookie.net/doodle-world/images/Snobat.png",
+    forms: ["Snobat", "Snovul", "Glacilisk"]
+  },
+  {
+    name: "Bunsweet Line",
+    value: 10,
+    group: "Swarms",
+    image: "https://static.wikia.nocookie.net/doodle-world/images/Bunsweet.png",
+    forms: ["Bunsweet", "Bunnybuns", "Cinnamoon"]
+  },
+  {
+    name: "Pebblet Line",
+    value: 10,
+    group: "Swarms",
+    image: "https://static.wikia.nocookie.net/doodle-world/images/Pebblet.png",
+    forms: ["Pebblet", "Rockodile"]
+  },
+  {
+    name: "Fluppy Line",
+    value: 10,
+    group: "Swarms",
+    image: "https://static.wikia.nocookie.net/doodle-world/images/Fluppy.png",
+    forms: ["Fluppy", "Fluppix"]
+  },
+  {
+    name: "Twiglet Line",
+    value: 10,
+    group: "Swarms",
+    image: "https://static.wikia.nocookie.net/doodle-world/images/Twiglet.png",
+    forms: ["Twiglet", "Branchkin", "Florush"]
+  }
+];
